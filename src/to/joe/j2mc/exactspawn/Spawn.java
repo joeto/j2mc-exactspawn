9c8db7e6735ef362e2ae7e14d5ef9864953f648b
mbax <matt@phozop.net>
2012-09-26 00:21:35 -0400

    whee

--- src/to/joe/j2mc/exactspawn/Spawn.java
+++ src/to/joe/j2mc/exactspawn/Spawn.java
@@ -1,9 +1,11 @@
 package to.joe.j2mc.exactspawn;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Spawn extends JavaPlugin implements Listener {
@@ -11,8 +13,18 @@ public class Spawn extends JavaPlugin implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void join(PlayerJoinEvent event) {
         if (!event.getPlayer().hasPlayedBefore()) {
-            event.getPlayer().teleport(event.getPlayer().getWorld().getSpawnLocation());
+            final Player player = event.getPlayer();
+            this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                public void run() {
+                    player.teleport(player.getWorld().getSpawnLocation());
+                }
+            });
         }
+    }
+
+    @EventHandler
+    public void respawn(PlayerRespawnEvent event) {
+        event.setRespawnLocation(event.getRespawnLocation().getWorld().getSpawnLocation());
     }
 
     @Override
