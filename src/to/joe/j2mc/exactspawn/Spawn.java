24391a28b67f4ae810d33963ace667c1cbba0c9c
mbax <matt@phozop.net>
2013-01-01 16:36:02 -0500

    Cleanup

--- src/to/joe/j2mc/exactspawn/Spawn.java
+++ src/to/joe/j2mc/exactspawn/Spawn.java
@@ -1,5 +1,6 @@
 package to.joe.j2mc.exactspawn;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,7 +9,11 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import to.joe.j2mc.core.J2MC_Manager;
+
 public class Spawn extends JavaPlugin implements Listener {
+
+    private int id = -1;
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void join(PlayerJoinEvent event) {
@@ -16,7 +21,11 @@ public class Spawn extends JavaPlugin implements Listener {
             final Player player = event.getPlayer();
             this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
                 public void run() {
-                    player.teleport(player.getWorld().getSpawnLocation());
+                    Location target = player.getWorld().getSpawnLocation();
+                    if (Spawn.this.id == 0) {
+                        target.setYaw(225);
+                    }
+                    player.teleport(target);
                 }
             });
         }
@@ -32,6 +41,7 @@ public class Spawn extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
+        this.id = J2MC_Manager.getServerID();
     }
 
 }
